f41d83a9bab4a1990e2fae20e1a2085f0a1b9492
Davide Radaelli <daviderad@outlook.com>
2020-02-04 05:54:19 -0500

    added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 // const HatsPage = () => (
 //   <div>
 //     <h1>HAT</h1>
@@ -13,6 +13,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
