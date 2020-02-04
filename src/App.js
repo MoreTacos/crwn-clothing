e132aac1640bf4dfe9e2816a36d9efdf8c01756b
Davide Radaelli <daviderad@outlook.com>
2020-02-04 06:16:40 -0500

    added stuff

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 // const HatsPage = () => (
 //   <div>
 //     <h1>HAT</h1>
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
