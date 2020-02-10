2efdc41f1539145014da7b32d354e8aa3fa9a67d
Davide Radaelli <daviderad@outlook.com>
2020-02-10 05:29:18 -0500

    added

--- src/App.js
+++ src/App.js
@@ -24,17 +24,12 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            },
-            () => {
-              console.log(this.state);
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
             }
-          );
+          });
         });
       } else {
         this.setState({ currentUser: userAuth });
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-up.styles.scss';
-
+import '../../components/sign-up/sign-up.component';
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
