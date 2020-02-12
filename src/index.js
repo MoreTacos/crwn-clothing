2c3c583f2cd16ad98f94d368d498c465e8d3c831
Davide Radaelli <daviderad@outlook.com>
2020-02-12 05:50:59 -0500

    added redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 // import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+
+import store from './redux/store';
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
